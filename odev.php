acdd9d5ba1041b1bd9f743cc72cf4bfc3ba0504f
gulden torun <guldentorn@gmail.com>
2023-01-03 16:12:47 +0300

    Create odev.php

---
+++ odev.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/*function check($value)
+{
+    $firstValue = $value;
+    if ($value == "") {
+        $result = "Değer boş olamaz.";
+    } else {
+        if ($value % 3 == 0) {
+            $result = "Girilen değer 3 ile bölünebiliyor.";
+        } else {
+            $value++;
+            while ($value % 3 != 0) {
+                $value++;
+            }
+            $result = "<b>Girilen Sayı : " . $firstValue . "</b><br> Girdiğiniz sayı 3 ile bölünemez. Bölünebilecek ilk sayı :" . $value;
+        }
+    }
+    return $result;
+}
+if(isset($_POST)){
+    check(@$_POST['number']);
+}*/
+
